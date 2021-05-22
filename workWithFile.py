159348a9b364e6d783790c44984acf1a0069f736
Nazim <66163226+gingersamurai@users.noreply.github.com>
2021-05-22 22:17:14 +0300

    v1.3.2 now the user has the ability to create their own tasks. True, for now they will simply be stored in a file :)

--- workWithFile.py
+++ workWithFile.py
@@ -2,6 +2,6 @@ sepstr = ' @#$ '
 
 
 def addToFile(statement, ans):
-    file = open("userTask.txt", "a+")
+    file = open("userTask.txt", "a+", encoding="utf-8")
     file.write(statement + sepstr + ans + '\n')
     file.close()
